7c9cb36fccd42b4521239f5beefeb1a930918bd0
Grant Elliott <grant@grantelliott.ca>
2020-09-04 00:51:35 -0400

    Remove Home Fragment text Set home nav icon to Raspberry Pi logo

--- app/src/main/java/ca/grantelliott/audiogeneapp/ui/home/HomeFragment.kt
+++ app/src/main/java/ca/grantelliott/audiogeneapp/ui/home/HomeFragment.kt
@@ -10,22 +10,11 @@ import androidx.fragment.app.viewModels
 import ca.grantelliott.audiogeneapp.R
 
 class HomeFragment : Fragment() {
-
-    private val homeViewModel: HomeViewModel by viewModels()
-
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val textView: TextView = view.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, {
-            textView.text = it
-        })
     }
 }
