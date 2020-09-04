71c0dc335c40eece2fcd1c1558ed8742793b359a
Grant Elliott <grant@grantelliott.ca>
2020-09-04 15:06:06 -0400

    Set system and service status on nav bar as badges

--- app/src/main/java/ca/grantelliott/audiogeneapp/ui/rpi/RpiStatusFragment.kt
+++ app/src/main/java/ca/grantelliott/audiogeneapp/ui/rpi/RpiStatusFragment.kt
@@ -5,11 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import ca.grantelliott.audiogeneapp.R
 import ca.grantelliott.audiogeneapp.ui.components.GaugeComponent
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import timber.log.Timber
@@ -31,14 +31,32 @@ class RpiStatusFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         Timber.d("+onViewCreated")
         super.onViewCreated(view, savedInstanceState)
-        val rpiStatusTextView: TextView = view.findViewById(R.id.text_rpi_status)
+        val navItem = activity?.findViewById<BottomNavigationView>(R.id.nav_view)
         val rpiCpuGaugeView: GaugeComponent = view.findViewById(R.id.rpi_cpu_usage_gauge)
         val rpiMemGaugeView: GaugeComponent = view.findViewById(R.id.rpi_mem_usage_gauge)
         val rpiCpuTempView: GaugeComponent = view.findViewById(R.id.rpi_cpu_temp_gauge)
 
         lifecycleScope.launchWhenCreated {
             viewModel.status().observe(viewLifecycleOwner, {
-                rpiStatusTextView.text = it.connectionStatus
+                navItem?.getOrCreateBadge(R.id.navigation_home)?.backgroundColor = when (it.connectionStatus) {
+                    "Connected" -> activity?.getColor(R.color.colorGaugeGood)!!
+                    else -> activity?.getColor(R.color.colorGaugeCritical)!!
+                }
+                if (it.systemStatus != null) {
+                    navItem?.getOrCreateBadge(R.id.navigation_genetics)?.backgroundColor =
+                        when (it.systemStatus?.audiogene) {
+                            "Running" -> activity?.getColor(R.color.colorGaugeGood)!!
+                            else -> activity?.getColor(R.color.colorGaugeCritical)!!
+                        }
+                    navItem?.getOrCreateBadge(R.id.navigation_supercollider)?.backgroundColor =
+                        when (it.systemStatus?.scsynth) {
+                            "Running" -> activity?.getColor(R.color.colorGaugeGood)!!
+                            else -> activity?.getColor(R.color.colorGaugeCritical)!!
+                        }
+                } else {
+                    navItem?.getOrCreateBadge(R.id.navigation_genetics)?.backgroundColor = activity?.getColor(R.color.colorGaugeCritical)!!
+                    navItem?.getOrCreateBadge(R.id.navigation_supercollider)?.backgroundColor = activity?.getColor(R.color.colorGaugeCritical)!!
+                }
                 rpiCpuGaugeView.value = it.cpuUsage
                 rpiMemGaugeView.value = it.memUsage
                 rpiCpuTempView.value = it.cpuTemp
