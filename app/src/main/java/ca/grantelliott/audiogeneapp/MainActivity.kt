df7a993a2fa4fcba3e08e8d9cbccd81890f59561
Grant Elliott <grant@grantelliott.ca>
2020-09-04 12:03:01 -0400

    Tweak styling and layout

--- app/src/main/java/ca/grantelliott/audiogeneapp/MainActivity.kt
+++ app/src/main/java/ca/grantelliott/audiogeneapp/MainActivity.kt
@@ -3,7 +3,7 @@ package ca.grantelliott.audiogeneapp
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -16,8 +16,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
 
-        val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
